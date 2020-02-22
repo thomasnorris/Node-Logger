b2685ce57dfd8f663fe275d6817aa6584e49e3d9
Thomas Norris <tom.norris135@gmail.com>
2020-02-22 14:58:57 -0500

    exports the config; successfully executes the stored proc

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var _fs = require('fs');
 const CFG_FILE = './config/config.json';
 var _cfg = readJson(CFG_FILE);
 
-var _pool = new _sql.ConnectionPool(_cfg.sql.connection);
+var _pool = new _sql.ConnectionPool(_cfg.connection);
 
 module.exports = {
+    Config: _cfg,
     Connect: async function() {
         return new Promise((resolve, reject) => {
             _pool.connect()
@@ -28,8 +29,38 @@ module.exports = {
                 });
         });
     },
-    Log: async function() {
+    Log: async function(primaryAppName, message, logTypeID, secondaryAppName) {
         return new Promise((resolve, reject) => {
+            (async () => {
+                var sp = _cfg.sp.log_nodejs_app;
+                var params = sp.params;
+                var request = _pool.request();
+
+                request.input(params.primary_app_name, _sql.VarChar(_sql.MAX), primaryAppName)
+                    .input(params.message, _sql.VarChar(_sql.MAX), message)
+                    .input(params.log_type_ID, _sql.Int, logTypeID || _cfg.log_types.info)
+                    .input(params.secondary_app_name, _sql.VarChar(_sql.MAX), secondaryAppName || undefined);
+
+                this.Connect()
+                    .then(() => {
+                        request.execute(sp.name)
+                            .then(() => {
+                                this.Disconnect()
+                                    .then(() => {
+                                        resolve('Execution successful.');
+                                    })
+                                    .catch((err) => {
+                                        reject('Disconnect error: ' + err);
+                                    });
+                            })
+                            .catch((err) => {
+                                reject('SP execution error: ' + err);
+                            });
+                    })
+                    .catch((err) => {
+                        reject('Connection error: ' + err);
+                    });
+            })();
         });
     }
 }
--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 
-var _log = require('./app.js');
+var _logger = require('./app.js');
 
 (async () => {
-    _log.Connect()
+    _logger.Log('Test.js', 'testing logging from app')
         .then((msg) => {
             console.log(msg);
         })
         .catch((err) => {
             console.log(err);
         });
+
 })();
