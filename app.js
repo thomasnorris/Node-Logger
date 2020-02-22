ebebab504209497cb0ac9760998d1ecce978b160
Thomas Norris <tom.norris135@gmail.com>
2020-02-22 16:14:20 -0500

    rearranged some things; added default primary app name

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
-var _sql = require('mssql');
-var _fs = require('fs');
-
 const CFG_FILE = './config/config.json';
 var _cfg = readJson(CFG_FILE);
+var _repoName = require('git-repo-name').sync();
 
+
+var _sql = require('mssql');
 var _pool = new _sql.ConnectionPool(_cfg.connection);
 
-var _exports = module.exports = {
-    LogDebug: async function(primaryAppName, message, secondaryAppName) {
+module.exports = {
+    LogDebug: async function(message, primaryAppName = null, secondaryAppName = null) {
         return new Promise((resolve, reject) => {
-            executeLogging(primaryAppName, message, _cfg.log_types.debug, secondaryAppName)
+            executeLogging(message, primaryAppName, secondaryAppName, _cfg.log_types.debug)
                 .then((msg) => {
                     resolve(msg);
                 })
@@ -19,9 +19,9 @@ var _exports = module.exports = {
                 });
         });
     },
-    LogInfo: async function(primaryAppName, message, secondaryAppName) {
+    LogInfo: async function(message, primaryAppName = null, secondaryAppName = null) {
         return new Promise((resolve, reject) => {
-            executeLogging(primaryAppName, message, _cfg.log_types.info, secondaryAppName)
+            executeLogging(message, primaryAppName, secondaryAppName, _cfg.log_types.info)
                 .then((msg) => {
                     resolve(msg);
                 })
@@ -30,9 +30,9 @@ var _exports = module.exports = {
                 });
         });
     },
-    LogWarning: async function(primaryAppName, message, secondaryAppName) {
+    LogWarning: async function(message, primaryAppName = null, secondaryAppName = null) {
         return new Promise((resolve, reject) => {
-            executeLogging(primaryAppName, message, _cfg.log_types.warning, secondaryAppName)
+            executeLogging(message, primaryAppName, secondaryAppName, _cfg.log_types.warning)
                 .then((msg) => {
                     resolve(msg);
                 })
@@ -41,9 +41,9 @@ var _exports = module.exports = {
                 });
         });
     },
-    LogError: async function(primaryAppName, message, secondaryAppName) {
+    LogError: async function(message, primaryAppName = null, secondaryAppName = null) {
         return new Promise((resolve, reject) => {
-            executeLogging(primaryAppName, message, _cfg.log_types.error, secondaryAppName)
+            executeLogging(message, primaryAppName, secondaryAppName, _cfg.log_types.error)
                 .then((msg) => {
                     resolve(msg);
                 })
@@ -76,15 +76,17 @@ async function connectDB() {
     });
 }
 
+// if primaryAppName === undefined, the record will insert _repoName
 // if secondaryAppName === undefined, the record will insert NULL
-async function executeLogging(primaryAppName, message, logTypeID, secondaryAppName) {
+async function executeLogging(message, primaryAppName, secondaryAppName, logTypeID) {
     return new Promise((resolve, reject) => {
         (async () => {
             var sp = _cfg.sp.log_nodejs_app;
             var params = sp.params;
-            var request = _pool.request();
 
-            request.input(params.primary_app_name, _sql.VarChar(_sql.MAX), primaryAppName)
+            // build a request that will execute sp with params
+            var request = _pool.request();
+            request.input(params.primary_app_name, _sql.VarChar(_sql.MAX), primaryAppName || _repoName)
                 .input(params.message, _sql.VarChar(_sql.MAX), message)
                 .input(params.log_type_ID, _sql.Int, logTypeID)
                 .input(params.secondary_app_name, _sql.VarChar(_sql.MAX), secondaryAppName);
@@ -113,5 +115,5 @@ async function executeLogging(primaryAppName, message, logTypeID, secondaryAppNa
 }
 
 function readJson(filePath) {
-    return JSON.parse(_fs.readFileSync(filePath, 'utf8'));
+    return JSON.parse(require('fs').readFileSync(filePath, 'utf8'));
 }
--- test.js
+++ test.js
@@ -1,2 +1,23 @@
 
 var _logger = require('./app.js');
+
+(function() {
+    // _logger.LogDebug('testing debug, no primary, no secondary');
+    // _logger.LogError('testing error, no primary, no secondary');
+    // _logger.LogInfo('testing info, no primary, no secondary');
+    // _logger.LogWarning('testing warning, no primary, no secondary');
+
+    _logger.LogDebug('testing debug, primary, no secondary', 'dummyPrimary')
+        .then(() => {
+            _logger.LogError('testing error, primary, no secondary', 'dummyPrimary');
+        });
+    // _logger.LogError('testing error, primary, no secondary', 'dummyPrimary');
+    // _logger.LogInfo('testing info, primary, no secondary', 'dummyPrimary');
+    // _logger.LogWarning('testing warning, primary, no secondary', 'dummyPrimary');
+
+    // _logger.LogDebug('testing debug, primary, secondary', 'dummyPrimaryName', 'dummySecondary');
+    // _logger.LogError('testing error, primary, secondary', 'dummyPrimary', 'dummySecondary');
+    // _logger.LogInfo('testing info, primary, secondary', 'dummyPrimary', 'dummySecondary');
+    // _logger.LogWarning('testing warning, primary, secondary', 'dummyPrimary', 'dummySecondary');
+
+})();
