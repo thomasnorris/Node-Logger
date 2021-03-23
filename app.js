877be2218a15f50fceb01534c64e10728c53a4b5
Thomas Norris <tom.norris135@gmail.com>
2021-03-23 14:46:03 -0400

    simplify building functions; remove unnecessary async labels

--- app.js
+++ app.js
@@ -30,156 +30,67 @@ process.on('unhandledRejection', (rejection) => {
         });
 });
 
+module.exports = {};
+
 // Log that the app has started
 executeLog(_cfg.messages.init, '', _cfg.log_types.init);
 
-module.exports = {
-    Init: {
-        Async: function(message, details) {
-            this.Sync(message, details)
-                .then(() => {
-                })
-                .catch(() => {
-                });
-        },
-        Sync: async function(message, details) {
-            return new Promise((resolve, reject) => {
-                executeLog(message, details, _cfg.log_types.init)
-                    .then((msg) => {
-                        if (_cfg.debug_mode)
-                            console.log(msg);
-                        resolve(msg);
-                    })
-                    .catch((err) => {
-                        if (_cfg.debug_mode)
-                            console.log(err);
-                        reject(msg);
-                    });
-            });
-        }
-    },
-    Debug: {
-        Async: function(message, details) {
-            this.Sync(message, details)
-                .then(() => {
-                })
-                .catch(() => {
-                });
-        },
-        Sync: async function(message, details) {
-            return new Promise((resolve, reject) => {
-                executeLog(message, details, _cfg.log_types.debug)
-                    .then((msg) => {
-                        if (_cfg.debug_mode)
-                            console.log(msg);
-                        resolve(msg);
-                    })
-                    .catch((err) => {
-                        if (_cfg.debug_mode)
-                            console.log(err);
-                        reject(msg);
-                    });
-            });
-        }
-    },
-    Info: {
-        Async: function(message, details) {
-            this.Sync(message, details)
-                .then((msg) => {
-                })
-                .catch((err) => {
-                });
-        },
-        Sync: async function(message, details) {
-            return new Promise((resolve, reject) => {
-                executeLog(message, details, _cfg.log_types.info)
-                    .then((msg) => {
-                        if (_cfg.debug_mode)
-                            console.log(msg);
-                        resolve(msg);
-                    })
-                    .catch((err) => {
-                        if (_cfg.debug_mode)
-                            console.log(err);
-                        reject(msg);
-                    });
-            });
-        }
-    },
-    Warning: {
-        Async: function(message, details) {
-            this.Sync(message, details)
-                .then((msg) => {
-                })
-                .catch((err) => {
-                });
-        },
-        Sync: async function(message, details) {
-            return new Promise((resolve, reject) => {
-                executeLog(message, details, _cfg.log_types.warning)
-                    .then((msg) => {
-                        if (_cfg.debug_mode)
-                            console.log(msg);
-                        resolve(msg);
-                    })
-                    .catch((err) => {
-                        if (_cfg.debug_mode)
-                            console.log(err);
-                        reject(msg);
-                    });
-            });
-        }
-    },
-    Error: {
-        Async: function(message, details) {
-            this.Sync(message, details)
-                .then((msg) => {
-                })
-                .catch((err) => {
-                });
-        },
-        Sync: async function(message, details) {
-            return new Promise((resolve, reject) => {
-                executeLog(message, details, _cfg.log_types.error)
-                    .then((msg) => {
-                        if (_cfg.debug_mode)
-                            console.log(msg);
-                        resolve(msg);
-                    })
-                    .catch((err) => {
-                        if (_cfg.debug_mode)
-                            console.log(err);
-                        reject(err);
-                    });
-            });
-        }
-    },
-}
+// build the functions for each Sync and Async method
+let types = Object.keys(_cfg.log_types);
+types.forEach((type, i) => {
+    let prop = {};
+    let name = type.slice(0, 1).toUpperCase() + type.slice(1, type.length);
 
-async function executeLog(message = _cfg.messages.default, details = '', logTypeID = _cfg.log_types.debug) {
+    prop[name] = {
+        Async: function(message, details) {
+            this.Sync(message, details)
+            .then(() => {
+            })
+            .catch(() => {
+            });
+        },
+        Sync: function(message, details) {
+            return new Promise((resolve, reject) => {
+                executeLog(message, details, types[type])
+                    .then((msg) => {
+                        if (_cfg.debug_mode)
+                            console.log(msg);
+                        resolve(msg);
+                    })
+                    .catch((err) => {
+                        if (_cfg.debug_mode)
+                            console.log(err);
+                        reject(msg);
+                    });
+            });
+        }
+    };
+
+    Object.assign(module.exports, prop);
+});
+
+function executeLog(message = _cfg.messages.default, details = '', logTypeID = _cfg.log_types.debug) {
     return new Promise((resolve, reject) => {
         if (details)
             details = JSON.stringify(details);
 
-        (async () => {
-            _pool.getConnection((err, connection) => {
-                if (err)
-                    resolve(err);
-                else {
-                    var query = 'call ' + _cfg.sql.connection.database + '.' + _cfg.sql.sp.log_node_app + '(';
-                    query += connection.escape(_cfg.app_ID) + ', ' + connection.escape(logTypeID) + ', ';
-                    query += connection.escape(message) + ', ' + connection.escape(details)  + ');';
+        _pool.getConnection((err, connection) => {
+            if (err)
+                resolve(err);
+            else {
+                var query = 'call ' + _cfg.sql.connection.database + '.' + _cfg.sql.sp.log_node_app + '(';
+                query += connection.escape(_cfg.app_ID) + ', ' + connection.escape(logTypeID) + ', ';
+                query += connection.escape(message) + ', ' + connection.escape(details)  + ');';
 
-                    connection.query(query, (err, res, fields) => {
-                        connection.release();
-                        if (err)
-                            resolve(err);
-                        else
-                            resolve(res);
-                    });
-                }
-            });
-        })();
+                connection.query(query, (err, res, fields) => {
+                    connection.release();
+                    if (err)
+                        resolve(err);
+                    else
+                        resolve(res);
+                });
+            }
+        });
     });
 }
 
